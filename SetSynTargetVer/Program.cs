77cb4b89bef4e539bcf8f7fddded71230ccec89f
Ace Olszowka <aolszowka@gmail.com>
2018-11-02 09:01:31 -0600

    Initial commit of the SetSynTargetVer Tool

---
+++ SetSynTargetVer/Program.cs
@@ -0,0 +1,80 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Ace Olszowka">
+//  Copyright (c) Ace Olszowka 2018. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace SetSynTargetVer
+{
+    using System;
+    using System.IO;
+    using NDesk.Options;
+
+    /// <summary>
+    /// Utility Program to Set Synergy Target Versions.
+    /// </summary>
+    class Program
+    {
+        const string PROGRAM_NAME = "SetSynTargetVer";
+
+        static int Main(string[] args)
+        {
+            string rootDirectory = string.Empty;
+            string targetRuntime = string.Empty;
+            string languageCompatibilityLevel = string.Empty;
+            bool showHelp = false;
+
+            // Parse our Options
+            OptionSet options = new OptionSet()
+            {
+                { "Root|R=", "The {ROOT} directory to scan for projects to modify.", rt => rootDirectory = rt },
+                { "TargetRuntime|TR=","(Optional) The {TARGETRUNTIME} to set projects to.", tr => targetRuntime = tr },
+                { "LanguageCompatibilityLevel|LCL=", "(Optional) The {LanguageCompatibilityLevel} to set projects to.", lcl => languageCompatibilityLevel = lcl },
+                { "help|?", "Show Help", h => showHelp = h != null }
+            };
+
+            try
+            {
+                options.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Write($"{PROGRAM_NAME}: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine($"Try `{PROGRAM_NAME} --help' for more information.");
+                return 1;
+            }
+
+            if (showHelp)
+            {
+                return _ShowHelp(options);
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(rootDirectory) || !Directory.Exists(rootDirectory))
+                {
+                    Console.WriteLine($"You must provide a valid directory for this tool. Try `{PROGRAM_NAME} --help' for more information.");
+
+                    // Return ERROR_PATH_NOT_FOUND
+                    return 3;
+                }
+                else
+                {
+                    return SetSynTargetVer.ProcessDirectory(rootDirectory, targetRuntime, languageCompatibilityLevel);
+                }
+            }
+        }
+
+        private static int _ShowHelp(OptionSet options)
+        {
+            Console.WriteLine($"Usage: {PROGRAM_NAME} [OPTIONS]");
+            Console.WriteLine("Assists in setting various Synergy Target Framework options in multiple SYNPROJ files.");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            options.WriteOptionDescriptions(Console.Out);
+            Console.WriteLine();
+            Console.WriteLine("See https://www.synergex.com/docs/#tools/toolsChap1Compilingatraditionalsynergyroutine.htm for Valid Values (Under Runtime Targeting)");
+            return 1;
+        }
+    }
+}
---
+++ SetSynTargetVer/SetSynTargetVer.cs
@@ -0,0 +1,77 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SetSynTargetVer.cs" company="Ace Olszowka">
+//  Copyright (c) Ace Olszowka 2018. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace SetSynTargetVer
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading.Tasks;
+    using System.Xml.Linq;
+
+    public static class SetSynTargetVer
+    {
+        internal static XNamespace msbuildNS = @"http://schemas.microsoft.com/developer/msbuild/2003";
+
+        public static void SetTargetRuntimeLevel(XDocument projXml, string targetRuntimeLevel)
+        {
+            // Find all TargetRuntimeLevel Tags
+            IEnumerable<XElement> targetRuntimeLevelTags = projXml.Descendants(msbuildNS + "TargetRuntimeLevel");
+
+            foreach (XElement targetRuntimeLevelTag in targetRuntimeLevelTags)
+            {
+                targetRuntimeLevelTag.Value = targetRuntimeLevel;
+            }
+        }
+
+        public static void SetLanguageCompatibilityLevel(XDocument projXml, string languageCompatibilityLevel)
+        {
+            // Find all LanguageCompatibilityLevel Tags
+            IEnumerable<XElement> languageCompatibilityLevelTags = projXml.Descendants(msbuildNS + "LanguageCompatibilityLevel");
+
+            foreach (XElement languageCompatibilityLevelTag in languageCompatibilityLevelTags)
+            {
+                languageCompatibilityLevelTag.Value = languageCompatibilityLevel;
+            }
+        }
+
+        internal static int ProcessDirectory(string targetDirectory, string targetRuntime, string languageCompatibilityLevel)
+        {
+            // Get All Synproj Files
+            IEnumerable<string> synprojFiles = Directory.EnumerateFiles(targetDirectory, "*.synproj", SearchOption.AllDirectories);
+
+            Parallel.ForEach(synprojFiles, synprojFile =>
+            {
+                XDocument projXml = XDocument.Load(synprojFile);
+
+                // Make a string copy to see if anything changed; in theory
+                // we could have used the "Changed" event but because of the
+                // way we always reset the values we need to instead compare
+                // the end result
+                string deepProjXmlCopy = projXml.ToString();
+
+                if (!string.IsNullOrWhiteSpace(targetRuntime))
+                {
+                    SetTargetRuntimeLevel(projXml, targetRuntime);
+                }
+
+                if (!string.IsNullOrWhiteSpace(languageCompatibilityLevel))
+                {
+                    SetLanguageCompatibilityLevel(projXml, languageCompatibilityLevel);
+                }
+
+                bool hasChanges = projXml.ToString() != deepProjXmlCopy;
+
+                if (hasChanges)
+                {
+                    projXml.Save(synprojFile);
+                }
+            }
+            );
+
+            return 0;
+        }
+    }
+}
